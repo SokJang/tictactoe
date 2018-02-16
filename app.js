ded84bb7fbf9d8e339b94d72feb502a750139342
SokJang <s.jang@sokdesign.de>
2018-02-16 18:39:59 +0100

    project setup, view template, styling template

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// settings port, data, template engine
+app.set('port', process.env.PORT || 3000 );
+app.set('view engine', 'ejs');
+
+// use static assets
+app.use(express.static('public'));
+
+// use routes defined in modules
+app.use(require('./routes/index'));
+
+// server
+app.listen(app.get('port'));
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// routing method
+router.get('/', function(req, res) {
+  const pagetitle = 'Tic Tac Toe';
+  // render from path
+  res.render('pages/index', {
+  	pagetitle: pagetitle
+  });
+});
+
+module.exports = router;
