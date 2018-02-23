0e6414dea7053376027c87586c2026357898627b
SokJang <s.jang@sokdesign.de>
2018-02-23 11:13:44 +0100

    getPlayedFields array with klicked states

--- modules/tictactoe/tictactoe.js
+++ modules/tictactoe/tictactoe.js
@@ -6,56 +6,10 @@ const plImg = 'http://localhost:3000/img/cross.svg'
 const aiImg = 'http://localhost:3000/img/heart.svg'
 const gamewin = [
   // winning rows
-	[0, 1, 2], 
-	[3, 4, 5],
-	[6, 7, 8],
-	// winning cols
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-  // winning diagonal
-  [0, 4, 8],
-	[2, 4, 6]
+	[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
 ]
 
-// gamestate
-let gamestate = () => {
-	
-	let player = []
-	let ai = []
-  
-  
-	let rows = tbod[0].rows
-	let rowcount = rows.length
-	let cells, cellcount, cell
-
-
-  for(let i =0; i < rowcount; i++){
-  	cells = rows[i].cells
-  	cellcount = cells.length
-
-    for (let j = 0; j < cellcount; j++){
-    	cell = cells[j];
-
-    	if (cell.classList[0] == 'ai'){
-        ai.push(j)
-        console.log(ai)
-    	  console.log('ai')
-
-    	} else if (cell.classList[0] == 'player'){
-        player.push(j)
-        console.log(player)
-    	  console.log('player')
-    	} else {
-    		console.log(cell)
-    	  console.log('default')
-    	}
-    	
-    }
-  }
-
-}
-
+let beispiel = ["ai", "ai", "ai", "ai", "player", "player", "player", "player", "player"]
 
 // reset
 const resetFields = () => {
@@ -66,10 +20,25 @@ const resetFields = () => {
     fields[i].setAttribute('class', 'default')
   }
 }
+
 // getter
+let getPlayedFields = () => {
+	let i = 0
+	let played = []
+  for(i; i < fields.length; i++){
+  	if (fields[i].classList == 'player')
+      played[i] = 'player'
+    if (fields[i].classList == 'ai')
+      played[i] = 'ai'
+    if (fields[i].classList == 'default')
+      played[i] = 'default'
+    console.log(played)
+  }
+}
 let getPlayerFields = () => {
   let i = 0
-  let player = document.querySelectorAll('.player');
+  let playerFields = []
+  let player = document.querySelectorAll('.ai');
   for(i; i < player.length; i++){
     i
   }
@@ -102,7 +71,9 @@ let setPlayerField = () => {
         this.setAttribute('data-counter', 1)
         this.setAttribute('class', 'player')
 
-        gamestate()
+        getPlayedFields()
+
+        //console.log(getMoves())
 
         if(getMoves()==9){
   	      console.log('game ended by player')
@@ -124,63 +95,63 @@ let setAiField = () => {
 	    fields[0].setAttribute('class', 'ai')
 	    fields[0].setAttribute('data-player', 'ai')
 	    fields[0].setAttribute('data-counter', 1)
-	    gamestate()
+	    //gamestate()
 	    break
 	  case fields[1].classList[0] !== 'ai' && fields[1].classList[0] !== 'player':
 	    imgs[1].setAttribute('src', aiImg)
 	    fields[1].setAttribute('class', 'ai')
 	    fields[1].setAttribute('data-player', 'ai')
 	    fields[1].setAttribute('data-counter', 1)
-	    gamestate()
+	    //gamestate()
 	    break
 	  case fields[2].classList[0] !== 'ai' && fields[2].classList[0] !== 'player':
 	    imgs[2].setAttribute('src', aiImg)
 	    fields[2].setAttribute('class', 'ai')
 	    fields[2].setAttribute('data-player', 'ai')
 	    fields[2].setAttribute('data-counter', 1)
-	    gamestate()
+	    //gamestate()
 	    break
 	  case fields[3].classList[0] !== 'ai' && fields[3].classList[0] !== 'player':
 	    imgs[3].setAttribute('src', aiImg)
 	    fields[3].setAttribute('class', 'ai')
 	    fields[3].setAttribute('data-player', 'ai')
 	    fields[3].setAttribute('data-counter', 1)
-	    gamestate()
+	    //gamestate()
 	    break
 	  case fields[4].classList[0] !== 'ai' && fields[4].classList[0] !== 'player':
 	    imgs[4].setAttribute('src', aiImg)
 	    fields[4].setAttribute('class', 'ai')
 	    fields[4].setAttribute('data-player', 'ai')
 	    fields[4].setAttribute('data-counter', 1)
-	    gamestate()
+	    //gamestate()
 	    break
 	  case fields[5].classList[0] !== 'ai' && fields[5].classList[0] !== 'player':
 	    imgs[5].setAttribute('src', aiImg)
 	    fields[5].setAttribute('class', 'ai')
 	    fields[5].setAttribute('data-player', 'ai')
 	    fields[5].setAttribute('data-counter', 1)
-	    gamestate()
+	    //gamestate()
 	    break
 	  case fields[6].classList[0] !== 'ai' && fields[6].classList[0] !== 'player':
 	    imgs[6].setAttribute('src', aiImg)
 	    fields[6].setAttribute('class', 'ai')
 	    fields[6].setAttribute('data-player', 'ai')
 	    fields[6].setAttribute('data-counter', 1)
-	    gamestate()
+	    //gamestate()
 	    break
 	  case fields[7].classList[0] !== 'ai' && fields[7].classList[0] !== 'player':
 	    imgs[7].setAttribute('src', aiImg)
 	    fields[7].setAttribute('class', 'ai')
 	    fields[7].setAttribute('data-player', 'ai')
 	    fields[7].setAttribute('data-counter', 1)
-	    gamestate()
+	    //gamestate()
 	    break
 	  case fields[8].classList[0] !== 'ai' && fields[8].classList[0] !== 'player':
 	    imgs[8].setAttribute('src', aiImg)
 	    fields[8].setAttribute('class', 'ai')
 	    fields[8].setAttribute('data-player', 'ai')
 	    fields[8].setAttribute('data-counter', 1)
-	    gamestate()
+	    //gamestate()
 	    break
 	  default:
 	    console.log('run gameEval by ai')
